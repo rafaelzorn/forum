5a8aa9a993f0eaf4fdad143ed1466fd736ec8927
rafaelzorn <rafael.zorn@gmail.com>
2019-01-17 21:06:41 -0200

    Tests user registration by administrator

--- tests/Feature/Manager/Users/UserTest.php
+++ tests/Feature/Manager/Users/UserTest.php
@@ -421,6 +421,315 @@ class UserTest extends TestCase
     }
 
     /** @test */
+    public function it_admin_cannot_edit_a_user_whose_name_exceeds_the_character_limit()
+    {
+        $user = factory(User::class)->create([
+            'name'                  => 'User One',
+            'email'                 => 'userone@userone.com.br',
+            'password'              => '123456',
+            'is_admin'              => true,
+            'active'                => true,
+        ]);
+
+        $response = $this->actingAs($this->admin)->from($this->userEditGetRoute($user->id))->put($this->userUpdateRoute($user->id), [
+            'name'                  => str_random(256),
+            'email'                 => 'userone@usertwo.com.br',
+            'password'              => '1234567',
+            'password_confirmation' => '1234567',
+            'is_admin'              => false,
+            'active'                => false,
+        ]);
+
+        $this->assertCount(1, $users = User::all());
+
+        $user = $users->first();
+
+        $this->assertEquals('User One', $user->name);
+        $this->assertEquals('userone@userone.com.br', $user->email);
+        $this->assertTrue(Hash::check('123456', Hash::make($user->password)));
+        $this->assertEquals(true, $user->is_admin);
+        $this->assertEquals(true, $user->active);
+
+        $response->assertRedirect($this->userEditGetRoute($user->id));
+        $response->assertSessionHasErrors(['name' => 'The name may not be greater than 255 characters.']);
+
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertTrue(session()->hasOldInput('is_admin'));
+        $this->assertTrue(session()->hasOldInput('active'));
+    }
+
+    /** @test */
+    public function it_admin_cannot_edit_a_user_without_email()
+    {
+        $user = factory(User::class)->create([
+            'name'                  => 'User One',
+            'email'                 => 'userone@userone.com.br',
+            'password'              => '123456',
+            'is_admin'              => true,
+            'active'                => true,
+        ]);
+
+        $response = $this->actingAs($this->admin)->from($this->userEditGetRoute($user->id))->put($this->userUpdateRoute($user->id), [
+            'name'                  => 'User Two',
+            'email'                 => '',
+            'password'              => '1234567',
+            'password_confirmation' => '1234567',
+            'is_admin'              => false,
+            'active'                => false,
+        ]);
+
+        $this->assertCount(1, $users = User::all());
+
+        $user = $users->first();
+
+        $this->assertEquals('User One', $user->name);
+        $this->assertEquals('userone@userone.com.br', $user->email);
+        $this->assertTrue(Hash::check('123456', Hash::make($user->password)));
+        $this->assertEquals(true, $user->is_admin);
+        $this->assertEquals(true, $user->active);
+
+        $response->assertRedirect($this->userEditGetRoute($user->id));
+        $response->assertSessionHasErrors(['email' => 'The email field is required.']);
+
+        $this->assertTrue(session()->hasOldInput('name'));
+        $this->assertTrue(session()->hasOldInput('is_admin'));
+        $this->assertTrue(session()->hasOldInput('active'));
+    }
+
+    /** @test */
+    public function it_admin_cannot_edit_a_user_whose_email_exceeds_the_character_limit()
+    {
+        $user = factory(User::class)->create([
+            'name'                  => 'User One',
+            'email'                 => 'userone@userone.com.br',
+            'password'              => '123456',
+            'is_admin'              => true,
+            'active'                => true,
+        ]);
+
+        $response = $this->actingAs($this->admin)->from($this->userEditGetRoute($user->id))->put($this->userUpdateRoute($user->id), [
+            'name'                  => 'User Two',
+            'email'                 => str_random(256),
+            'password'              => '1234567',
+            'password_confirmation' => '1234567',
+            'is_admin'              => false,
+            'active'                => false,
+        ]);
+
+        $this->assertCount(1, $users = User::all());
+
+        $user = $users->first();
+
+        $this->assertEquals('User One', $user->name);
+        $this->assertEquals('userone@userone.com.br', $user->email);
+        $this->assertTrue(Hash::check('123456', Hash::make($user->password)));
+        $this->assertEquals(true, $user->is_admin);
+        $this->assertEquals(true, $user->active);
+
+        $response->assertRedirect($this->userEditGetRoute($user->id));
+        $response->assertSessionHasErrors(['email' => 'The email may not be greater than 255 characters.']);
+
+        $this->assertTrue(session()->hasOldInput('name'));
+        $this->assertTrue(session()->hasOldInput('is_admin'));
+        $this->assertTrue(session()->hasOldInput('active'));
+    }
+
+    /* @test */
+    public function it_admin_can_not_edit_a_user_with_an_existing_email()
+    {
+        $anotherUser = factory(User::class)->create([
+            'email' => 'anotheruser@anotheruser.com.br'
+        ]);
+
+        $user = factory(User::class)->create([
+            'name'                  => 'User One',
+            'email'                 => 'userone@userone.com.br',
+            'password'              => '123456',
+            'is_admin'              => true,
+            'active'                => true,
+        ]);
+
+        $response = $this->actingAs($this->admin)->from($this->userEditGetRoute($user->id))->put($this->userUpdateRoute($user->id), [
+            'name'                  => 'User Two',
+            'email'                 => 'anotheruser@anotheruser.com.br',
+            'password'              => '1234567',
+            'password_confirmation' => '1234567',
+            'is_admin'              => false,
+            'active'                => false,
+        ]);
+
+        $this->assertCount(1, $users = User::all());
+
+        $user = $users->first();
+
+        $this->assertEquals('User One', $user->name);
+        $this->assertEquals('userone@userone.com.br', $user->email);
+        $this->assertTrue(Hash::check('123456', Hash::make($user->password)));
+        $this->assertEquals(true, $user->is_admin);
+        $this->assertEquals(true, $user->active);
+
+        $response->assertRedirect($this->userEditGetRoute($user->id));
+        $response->assertSessionHasErrors(['email' => 'The email has already been taken.']);
+
+        $this->assertTrue(session()->hasOldInput('name'));
+        $this->assertTrue(session()->hasOldInput('is_admin'));
+        $this->assertTrue(session()->hasOldInput('active'));
+    }
+
+    /* @test */
+    public function it_admin_cannot_edit_a_user_with_an_invalid_email()
+    {
+        $user = factory(User::class)->create([
+            'name'                  => 'User One',
+            'email'                 => 'userone@userone.com.br',
+            'password'              => '123456',
+            'is_admin'              => true,
+            'active'                => true,
+        ]);
+
+        $response = $this->actingAs($this->admin)->from($this->userEditGetRoute($user->id))->put($this->userUpdateRoute($user->id), [
+            'name'                  => 'User Two',
+            'email'                 => 'usertwo.com.br',
+            'password'              => '1234567',
+            'password_confirmation' => '1234567',
+            'is_admin'              => false,
+            'active'                => false,
+        ]);
+
+        $this->assertCount(1, $users = User::all());
+
+        $user = $users->first();
+
+        $this->assertEquals('User One', $user->name);
+        $this->assertEquals('userone@userone.com.br', $user->email);
+        $this->assertTrue(Hash::check('123456', Hash::make($user->password)));
+        $this->assertEquals(true, $user->is_admin);
+        $this->assertEquals(true, $user->active);
+
+        $response->assertRedirect($this->userEditGetRoute($user->id));
+        $response->assertSessionHasErrors(['email' => 'The email must be a valid email address.']);
+
+        $this->assertTrue(session()->hasOldInput('name'));
+        $this->assertTrue(session()->hasOldInput('is_admin'));
+        $this->assertTrue(session()->hasOldInput('active'));
+    }
+
+    /* @test */
+    public function it_admin_edit_a_user_without_password()
+    {
+        $user = factory(User::class)->create([
+            'name'                  => 'User One',
+            'email'                 => 'userone@userone.com.br',
+            'password'              => '123456',
+            'is_admin'              => true,
+            'active'                => true,
+        ]);
+
+        $response = $this->actingAs($this->admin)->put($this->userUpdateRoute($user->id), [
+            'name'                  => 'User Two',
+            'email'                 => 'usertwo@usertwo.com.br',
+            'password'              => '',
+            'password_confirmation' => '',
+            'is_admin'              => false,
+            'active'                => false,
+        ]);
+
+        $this->assertCount(1, $users = User::all());
+
+        $user = $users->first();
+
+        $this->assertEquals('User Two', $user->name);
+        $this->assertEquals('usertwo@usertwo.com.br', $user->email);
+        $this->assertTrue(Hash::check('1234567', $user->password));
+        $this->assertEquals(false, $user->is_admin);
+        $this->assertEquals(false, $user->active);
+
+        $response->assertRedirect($this->userIndexGetRoute());
+        $response->assertSessionHas('message', [
+            'type' => 'success',
+            'message' => 'User successfully updated.',
+        ]);
+    }
+
+    /* @test */
+    public function it_admin_cannot_edit_a_user_whose_password_doesnt_have_the_minimum_of_characters()
+    {
+        $user = factory(User::class)->create([
+            'name'                  => 'User One',
+            'email'                 => 'userone@userone.com.br',
+            'password'              => '123456',
+            'is_admin'              => true,
+            'active'                => true,
+        ]);
+
+        $response = $this->actingAs($this->admin)->from($this->userEditGetRoute($user->id))->put($this->userUpdateRoute($user->id), [
+            'name'                  => 'User Two',
+            'email'                 => 'usertwo@usertwo.com.br',
+            'password'              => '1234',
+            'password_confirmation' => '1234',
+            'is_admin'              => false,
+            'active'                => false,
+        ]);
+
+        $this->assertCount(1, $users = User::all());
+
+        $user = $users->first();
+
+        $this->assertEquals('User One', $user->name);
+        $this->assertEquals('userone@userone.com.br', $user->email);
+        $this->assertTrue(Hash::check('123456', Hash::make($user->password)));
+        $this->assertEquals(true, $user->is_admin);
+        $this->assertEquals(true, $user->active);
+
+        $response->assertRedirect($this->userEditGetRoute($user->id));
+        $response->assertSessionHasErrors(['password' => 'The password must be at least 6 characters.']);
+
+        $this->assertTrue(session()->hasOldInput('name'));
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertTrue(session()->hasOldInput('is_admin'));
+        $this->assertTrue(session()->hasOldInput('active'));
+    }
+
+    /* @test */
+    public function it_admin_cannot_edit_a_user_when_the_password_confirmation_doesnt_match()
+    {
+        $user = factory(User::class)->create([
+            'name'                  => 'User One',
+            'email'                 => 'userone@userone.com.br',
+            'password'              => '123456',
+            'is_admin'              => true,
+            'active'                => true,
+        ]);
+
+        $response = $this->actingAs($this->admin)->from($this->userEditGetRoute($user->id))->put($this->userUpdateRoute($user->id), [
+            'name'                  => 'User Two',
+            'email'                 => 'usertwo@usertwo.com.br',
+            'password'              => '123456',
+            'password_confirmation' => '1234567',
+            'is_admin'              => false,
+            'active'                => false,
+        ]);
+
+        $this->assertCount(1, $users = User::all());
+
+        $user = $users->first();
+
+        $this->assertEquals('User One', $user->name);
+        $this->assertEquals('userone@userone.com.br', $user->email);
+        $this->assertTrue(Hash::check('123456', Hash::make($user->password)));
+        $this->assertEquals(true, $user->is_admin);
+        $this->assertEquals(true, $user->active);
+
+        $response->assertRedirect($this->userEditGetRoute($user->id));
+        $response->assertSessionHasErrors(['password' => 'The password confirmation does not match.']);
+
+        $this->assertTrue(session()->hasOldInput('name'));
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertTrue(session()->hasOldInput('is_admin'));
+        $this->assertTrue(session()->hasOldInput('active'));
+    }
+
+    /** @test */
     public function it_admin_can_delete_a_user()
     {
         $user = factory(User::class)->create();
